3b7efcc86d97716bf77e9689ec6d7388f6d4061a
rogeriosouz <rogeriopedramartins@gmail.com>
2022-06-24 21:21:15 -0300

    concertando img de fundo subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Logo } from '../components/Logo';
 import { useCreateSubscriberMutation } from '../graphql/generated';
+import imgFundo from '/src/assets/coldeMocap.png';
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -74,7 +75,7 @@ export function Subscribe() {
           </form>
         </div>
       </div>
-      <img src="/src/assets/coldeMocap.png" className="mt-10" alt="" />
+      <img src={imgFundo} className="mt-10" alt="" />
     </div>
   );
 }
